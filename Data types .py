52788f54801268f68a4a0991f7c66a9a622e9930
Youssef <ya103857@gmail.com>
2026-06-27 23:00:03 +0300

    Add files via upload

---
+++ Data types .py
@@ -0,0 +1,18 @@
+print("All Data in Python is object")
+print("-"*30)
+
+print(type(5.7)) # float = Floating poimt number
+
+print(type(5)) # int = Integer
+
+print(type("youssef")) # str = String
+
+print(type([1, 2, 3])) # list = List
+
+print (type((1,2,3))) # tuple = Tuple
+
+print(type({"One" :1 , "Two" : 2 , "Three ":3})) # dict = Dictionary
+
+print(type({"Four" :4 , "Five" :5 ,"Six" :6})) # dict = Dictionary
+
+print (type(5==5)) # bool =# Boolean 
